6e93c60811377a7a57e1cb835d735619b1796f5e
Romy <romy.rivaldy@gmail.com>
2025-07-17 12:07:50 +0700

    Initial Commit

---
+++ src/main/java/com/rivaldy/creditservices/CreditServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rivaldy.creditservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivaldy/creditservices/CreditServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivaldy.creditservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
